b0a8ef4d8e180cae466b21c152fb36a32508864a
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2017-12-14 10:44:08 -1000

    fixed code space

--- Viewcrop.R
+++ Viewcrop.R
@@ -30,12 +30,12 @@
   Synonyms <- vws$gnrl[3,2]
   Common_names <- vws$gnrl[4,2]
   Editor <- vws$gnrl[5,2]
-  Ecocrop code <- vws$gnrl[6,2]
+  Ecocrop_code <- vws$gnrl[6,2]
   Notes <- vws$Notes[1,]
   Sources <- vws$Notes[2,]
   
   vewcrop <- list()
-  vewcrop <- c( Authority, Family, Synonym, Common_names, Editor, Ecocrop code, Notes, Sources)
+  vewcrop <- c( Authority, Family, Synonym, Common_names, Editor, Ecocrop_code, Notes, Sources)
 
   View(vws$Notes)
   
