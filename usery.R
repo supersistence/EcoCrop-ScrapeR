3b552ff03dbcb6c5d6fa732beb2f48bc29a05b0b
Hunter Heaivilin <33341498+supersistence@users.noreply.github.com>
2017-12-14 10:41:42 -1000

    use table scraper

--- usery.R
+++ usery.R
@@ -2,13 +2,14 @@
 
 
 ### Check for packages, install if necessary
-list.of.packages <- c("rvest", "magrittr", "tidyverse")
+list.of.packages <- c("rvest", "magrittr", "dplyr" "tidyverse")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 #Call package libraries
 library(rvest)
 library(magrittr)
+library(dplyr)
 library(tidyverse)
 
 
@@ -16,13 +17,20 @@ library(tidyverse)
 # Read entire list of crop datasheet urls into a variable
 urls <- read_lines("/Users/hunterheaivilin/GitHub/Data-Operations/datasheeturl.csv")
 
-yuse <- data_frame("MainUse", "DetailedUse", "UsedPart")
+# Remove header line from url list
+urls <- urls[-1]
+
+# Create a data frame
+yust <- data_frame()
 
 # for loop to move through through the url list
-for(i in (urls[3])) {
-  ##  url <- toString(urls[i])
+for(i in (urls[])) {
   html <- read_html(i)
-  species <- html_text(html_nodes(html, "h2"))
+  
+  # take url string 'i' split along character '='
+  brkr <- strsplit(i, "=") 
+  cropcode <- brkr[[1]][2]  #Crop code
+
 
   # Create empty list to add table data into
   uses_ls <- list()
@@ -43,10 +51,21 @@ for(i in (urls[3])) {
   #Remove first row
   uses_ls$Uses <- uses_ls$Uses[-1,]
   
+  # Adds new column with crop code
+  uses_ls$Uses$crop_code <- cropcode
+  
   # Add scraped use table to master use 'yuse' table
-  yuse <- rbind(yuse, uses_ls$Uses)
+  yust <- bind_rows(yust, uses_ls$Uses)
   
 }
 
-View(yuse)
+View(yust)
 View(uses_ls$Uses)
+
+
+
+
+#### Receviing error for blank tables
+# Error in `$<-.data.frame`(`*tmp*`, "crop_code", value = "299") : 
+# replacement has 1 row, data has 0
+
